4d887d47ce938aba5d05a13c19845ea5ebb0b2e3
Carlos Vieira <cadu.vieira2276@gmail.com>
2020-08-08 00:32:51 -0300

    Projeto relacionado a conta bancaria iniciando com os dados de deposito

---
+++ Conta_Bancaria/ContaBancaria.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Conta_Bancaria {
+    class ContaBancaria {
+        public int NumeroDaConta { get; private set; }
+        public string TitularDaConta { get; set; }
+        public double DepositoInicial { get; set; }
+        public double Saldo { get; private set; }
+
+        public double Imposto { get; private set; }
+
+        public ContaBancaria() {
+
+        }
+
+        public ContaBancaria(int numeroDaConta, string titular, double depositoInicial, double saldo, double imposto) {
+            NumeroDaConta = numeroDaConta;
+            TitularDaConta = titular;
+            DepositoInicial = depositoInicial;
+            Saldo = saldo;
+            Imposto = imposto;
+        }
+
+          
+       
+        
+        public void Deposito(double deposito) {
+            Saldo +=  deposito;
+        }
+
+        public void Saque(double saque) {
+            Saldo -= saque;
+
+        }
+         
+        
+        public void Taxa(double taxa) {
+            Saldo -= Imposto;                                               
+        }
+
+
+        public override string ToString() {
+            return "Conta " 
+                + NumeroDaConta + ", " 
+                + "Titular: " 
+                + TitularDaConta + ", " + "Saldo: " + "$ "
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+
+}
---
+++ Conta_Bancaria/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Globalization;
+
+namespace Conta_Bancaria {
+    class Program {
+        static void Main(string[] args) {
+
+                       
+            Console.Write("Entre o número da conta: ");
+            int numeroDaConta = int.Parse(Console.ReadLine());
+           
+            Console.Write("Entre o titular da conta: ");
+            string nome = Console.ReadLine();
+            
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char sim_Ou_Nao = char.Parse(Console.ReadLine());
+            
+            Console.Write("Entre o valor de depósito inicial: ");
+            double deposito_Inicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+
+            ContaBancaria conta = new ContaBancaria(numeroDaConta, nome, sim_Ou_Nao, deposito_Inicial, 5.00);
+
+            Console.WriteLine("Dados da conta:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+
+            Console.Write("Entre um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Deposito(deposito);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+            Console.WriteLine();
+            Console.Write("Entre um valor para saque: ");           
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            conta.Saque(saque);
+
+            conta.Taxa(5.00);
+            Console.WriteLine("Dados da conta atualizados:");
+            Console.WriteLine(conta);
+
+
+
+
+        }
+    }
+}
